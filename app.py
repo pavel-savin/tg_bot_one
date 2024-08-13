53ee8138d68a1003d8f9a23d0f9e56b4dccf18ce
Pavel savin <17.115@mail.ru>
2024-08-13 22:00:11 +0300

    Optimized currency withdrawal

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
 import telebot
 from config import TOKEN, keys
 from extensions import ConvertionException, CryptoConvertor
-
+import pymorphy3
 
 bot = telebot.TeleBot(TOKEN)
+morph = pymorphy3.MorphAnalyzer()
 
-
+def normal_str_output(curency_name, amount):
+    parsed_word = morph.parse(curency_name)[0]
+    
+    if int(amount) == 1:
+        case = 'nomn'
+    else:
+        case = 'gent'
+    
+    inflected = parsed_word.inflect({case})
+    
+    if inflected is None:
+        return curency_name
+    
+    return inflected.word
+    
 @bot.message_handler(commands=["start", "help"])  # Помощь
 def start_message(message: telebot.types.Message):
     text = "Чтобы начать работу введите комндау боту в следующем формате:\n<Имя валюты>\
@@ -33,7 +48,9 @@ def convert(message: telebot.types.Message):
 
     try:
         total_base = CryptoConvertor.get_price(quote, base, amount)
-        text = f"Цена {amount} {quote} в {base} - {total_base}"
+        quote_correct = normal_str_output(keys[quote], amount)
+        base_correct = normal_str_output(keys[base], amount)
+        text = f"Цена {amount} {quote_correct} в {base_correct} - {total_base}"
     except ConvertionException as e:
         text = f"Ошибка конвертации: {e}"
 
