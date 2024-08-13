f0f7016892f1ed970b711a6048e293b858e68396
Pavel savin <17.115@mail.ru>
2024-08-13 23:09:28 +0300

    new currency

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-TOKEN = "7186454751:AAFEUarpW1FxL0LPHOZuh7qXI9CFVB1U5WM"
+TOKEN = "7186454751:AAGnjRzeAaNJF4JcLHaGHfyEu2jXGCUEbVc"
 
 
 keys = {
@@ -6,5 +6,5 @@ keys = {
     "эфириум": "ETH",
     "доллар": "USD",
     "рубль": "RUB",
-    "евро": "EURO",
+    "солана": "SOL",
 }
